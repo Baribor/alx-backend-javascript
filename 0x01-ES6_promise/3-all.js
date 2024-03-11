6803d2974ba00ab7a67377d37e7180291fab97ba
Baribor Saturday <saturdaybaribor@gmail.com>
2024-03-11 17:15:38 +0100

    All done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { createUser, uploadPhoto } from './utils';
 
-export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((data) => {
+export default async function handleProfileSignup() {
+  await Promise.all([uploadPhoto(), createUser()]).then((data) => {
     const [{ body }, { firstName, lastName }] = data;
     console.log(body, firstName, lastName);
   }).catch(() => console.log('Signup system offline'));
